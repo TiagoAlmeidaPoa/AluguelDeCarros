f661f29d64e6a33a0e6ab243bc6d71e7f6cf3774
TiagoAlmeidaPoa <tiago.pipinha@gmail.com>
2019-12-04 16:40:09 -0200

    criei a classe ServicoDeTaxaDePoa que implementa a interface  servicoDeTaxa e preenche o metodo taxa de acordo com sua politica

---
+++ src/main/java/aluguelDeCarros/com/br/aluguelDeCarros/com/br/servico/ServicoDeTaxaDePoa.java
@@ -0,0 +1,17 @@
+package aluguelDeCarros.com.br.aluguelDeCarros.com.br.servico;
+
+//aqui estou implementando a classe ServicoDeTaxaPoa e preenchendo o metodo taxa de acordo 
+//com a politica de taxa de Poa, ou seja se o montante for menor ou igual a  100 é cobrada 
+//uma taxa de 20%, se não é cobrada uma taxa de 15%.
+
+public class ServicoDeTaxaDePoa implements ServicoDeTaxa{
+
+	public double taxa(double montante) {
+		if(montante <= 100) {
+			return montante * 0.2;
+		}else {
+			return montante * 0.15;
+		}
+	}
+
+}
